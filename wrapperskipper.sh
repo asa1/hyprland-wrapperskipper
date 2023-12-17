28008fb40b0691fff62fecc4aca15ca5b4f43d1e
Asa <asourdiffe@gmail.com>
2023-12-16 22:36:08 -0800

    Update wrapperskipper.sh
    clean up comments, simplify variables

--- wrapperskipper.sh
+++ wrapperskipper.sh
@@ -8,23 +8,20 @@
 # - Optionally, if a second argument 'with_window' is provided, it also moves the currently focused window to the target workspace.
 # Note: Ensure the 'max_workspaces' variable is set to the maximum number of workspaces for wrapping behavior.
 
-# Set a maximum workspace number for wrapping back to 1:
+# Set the maximum number of workspaces
 max_workspaces=7
 
 # Get the current workspace ID
 current_workspace=$(hyprctl activeworkspace -j | jq '.id')
 
-# Get a sorted list of workspaces with their window counts
-occupied_workspaces=$(hyprctl workspaces -j | jq -c '.[] | select(.windows > 0) | .id' | sort -n)
+# Get a sorted array of non-empty workspaces
+occupied_workspaces=($(hyprctl workspaces -j | jq -c '.[] | select(.windows > 0) | .id' | sort -n))
 
-# Convert occupied workspaces to an array
-occupied_workspace_array=($occupied_workspaces)
-
-# Determine the target workspace based on the argument
+# Determine the target workspace based on the argument next/prev/new
 if [ "$1" = "next" ]; then
   # Find the next occupied workspace or wrap around
   found=false
-  for workspace in "${occupied_workspace_array[@]}"; do
+  for workspace in "${occupied_workspaces[@]}"; do
     if [[ $workspace -gt $current_workspace ]]; then
       target_workspace=$workspace
       found=true
@@ -32,23 +29,23 @@ if [ "$1" = "next" ]; then
     fi
   done
   if [[ $found = false ]]; then
-    target_workspace=${occupied_workspace_array[0]}
+    target_workspace=${occupied_workspaces[0]}
   fi
 elif [ "$1" = "prev" ]; then
   # Find the previous occupied workspace or wrap around
-  for ((i=${#occupied_workspace_array[@]}-1; i>=0; i--)); do
-    if [[ ${occupied_workspace_array[i]} -lt $current_workspace ]]; then
-      target_workspace=${occupied_workspace_array[i]}
+  for ((i=${#occupied_workspaces[@]}-1; i>=0; i--)); do
+    if [[ ${occupied_workspaces[i]} -lt $current_workspace ]]; then
+      target_workspace=${occupied_workspaces[i]}
       break
     fi
   done
   if [[ -z $target_workspace ]]; then
-    target_workspace=${occupied_workspace_array[-1]}
+    target_workspace=${occupied_workspaces[-1]}
   fi
 elif [ "$1" = "new" ]; then
   # Find the next empty workspace, considering wrapping
   target_workspace=$current_workspace
-  while [[ " ${occupied_workspace_array[*]} " =~ " ${target_workspace} " ]] || [[ $target_workspace -eq 0 ]]; do
+  while [[ " ${occupied_workspaces[*]} " =~ " ${target_workspace} " ]] || [[ $target_workspace -eq 0 ]]; do
     target_workspace=$(( (target_workspace % max_workspaces) + 1 ))
   done
 else
